d503d66f679567ddd6037d3bd41300ada0b49b89
yunusemreaydinn <ea.emreaydn.58@gmail.com>
2023-08-08 16:43:06 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    NgIf,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {
+  input = '';
+  list: {task:string, editMode: boolean}[] = [];
+  editMode:boolean = false;
+  newTask:string = "";
+  index:number = 0;
+
+  addItem() {
+    this.list.push({ task: this.input, editMode: false });
+    this.input = '';
+  }
+
+  deleteItem(index: any) {
+    this.list.splice(index, 1);
+  }
+
+  changeMode(index:number){
+    this.list[index].editMode = !this.list[index].editMode;
+    this.newTask = this.list[index].task;
+    this.index = index;
+  }
+
+  saveNewTask(){
+    this.list[this.index].task = this.newTask;
+    this.list[this.index].editMode = !this.list[this.index].editMode;
+  }
+
+}
